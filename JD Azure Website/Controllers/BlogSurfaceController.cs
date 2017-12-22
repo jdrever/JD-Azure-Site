61b155c08163ec0ce1a972b4e43e859dd6c1d8a0
James Drever <james@jamesdrever.co.uk>
2017-12-22 18:16:37 +0000

    First draft of blog comment functionality.  Not yet functioning correctly.

---
+++ JD Azure Website/Controllers/BlogSurfaceController.cs
@@ -0,0 +1,40 @@
+﻿using JD_Azure_Website.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core.Services;
+using Umbraco.Web.Mvc;
+
+namespace JD_Azure_Website.Controllers
+{
+    public class BlogSurfaceController : SurfaceController
+    {
+        public ActionResult DisplayListOfComments()
+        {
+            return PartialView("JDListOfBlogComments", new BlogPostCommentViewModel());
+        }
+
+        public ActionResult PostComment(BlogPostCommentViewModel comment)
+        {
+            if (!ModelState.IsValid)
+            {
+                //Not valid - so lets return the user back to the view with the data they entered still prepopulated
+                return CurrentUmbracoPage();
+            }
+;
+            var commentNode = Services.ContentService.CreateContent(
+            "Comment made: "+DateTime.Now.ToLongDateString(), // Node Name - what I want to call the new child node
+            CurrentPage.Id, // Parent Node we want to add to
+            "JDBlogPostComment", // The alias of the Document Type
+            0); // Umbraco User ID this will be created by, default 0
+            commentNode.SetValue("comment", comment.Comment);
+            commentNode.SetValue("emailAddress", comment.EmailAddress);
+            commentNode.SetValue("name", comment.Name);
+            Services.ContentService.SaveAndPublishWithStatus(commentNode);
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
---
+++ JD Azure Website/Models/BlogPostCommentViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Umbraco.Core.Models;
+using Umbraco.Web.Models;
+
+namespace JD_Azure_Website.Models
+{
+    public class BlogPostCommentViewModel {  
+        //: RenderModel
+    //{
+    //    public BlogPostCommentViewModel(IPublishedContent content)
+    //        : base(content)
+     //   {
+     //   }
+
+        [Required]
+        public string Comment { get; set; }
+
+        public string Name { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
